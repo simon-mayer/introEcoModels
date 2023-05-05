acb7397f0da5e11a99cf9930d89a7e1ef3ea897d
Simon Mayer <simon.mayer@uni-bayreuth.de>
2023-05-05 16:39:38 +0200

    Project Structure, Base TabPanel UI and bare Logistic Model

--- R/logistisches_wachstum.R
+++ R/logistisches_wachstum.R
@@ -7,7 +7,7 @@
 
 #' Modellfunktion für diskretes logistisches Wachstum
 #'
-#' @param r Zahl zwischen 0 und 1, gibt Wachstumsrate an
+#' @param r Zahl zwischen -0.1 und 4, gibt Wachstumsrate an
 #' @param K Zahl, gibt Carrying Capacity der Umwelt an ~ max Population
 #' @param N Array von Zahlen, hält am Index 1 die Populationsgröße zur Zeit 1
 #' @param t Zahl Endzeitpunkt der Simulation
@@ -23,3 +23,41 @@ Nt <- function(r, K, N, t) { # Parameter
   }
   N
 }
+
+logisticUI <- function(id){
+  tabPanel("Logistic Model",
+           fluidRow(
+             column(6,
+              sliderInput(shiny::NS(id, "growth_rate"), "Wachstumsrate",
+                          value = 0.2, min = -0.1, max = 3.5, step = 0.1 ),
+              sliderInput(shiny::NS(id, "capacity"), "Kapazität",
+                          value = 100, min = 10, max = 1000, step = 10 )),
+              column(6,
+                      sliderInput(shiny::NS(id, "start"), "Anfangspopulation (Ratio zu Kap)",
+                          value = 0.1, min = 0, max = 2, step = 0.1),
+              sliderInput(shiny::NS(id, "end"), "Endzeitpunkt",
+                             value = 100, min = 10, max = 1000, step = 10))),
+           fluidRow(
+             column(12, plotOutput(NS(id, "population")))
+           )
+
+  )
+
+}
+
+
+
+logisticServer <- function(id){
+  moduleServer(id, function(input, output, session){
+      pop <- reactive(data.frame(Zeit=1:input$end, Population=Nt(input$growth_rate, input$capacity,
+                         input$start, input$end)))
+      output$population <- renderPlot({
+        ggplot2::ggplot(data = pop(), ggplot2::aes(x=Zeit, y=Population)) + ggplot2::geom_line()
+      })
+
+
+  })
+
+}
+
+
--- package_commands_history.R
+++ package_commands_history.R
@@ -14,3 +14,13 @@ document()
 ?Nt
 
 usethis::git_default_branch_configure()
+use_package("shiny")
+use_r("app.R")
+
+#05.05
+use_package("ggplot2")
+document()
+load_all()
+use_rstudio()
+
+
