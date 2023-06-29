a35412c8bcfa7101d6950350470d9f35fe6cfc7b
Simon Mayer <simon.mayer@uni-bayreuth.de>
2023-06-29 10:46:24 +0000

    deleted blue model, fixed axis at lotka

--- R/lotka-volterra.R
+++ R/lotka-volterra.R
@@ -28,8 +28,6 @@ LotVmod <- function (Time, State, Pars) {
   with(as.list(c(State, Pars)), {
     dB = r*B - a*B*R
     dR = -m*R + b*a*B*R
-    str(dB)
-    str(dR)
     # gibt Veränderungen der Populationen zurück
     return(list(c(dB, dR)))
   })
@@ -37,29 +35,29 @@ LotVmod <- function (Time, State, Pars) {
 
 
 lotkaVolterraUI <- function(id){
-  tabPanel("Lotka Volterra",
+  tabPanel("Lotka Volterra für Räuber/Beute",
            fluidRow(
              column(4,
-                    sliderInput(shiny::NS(id, "growth_rate_red"),
-                                "Wachstumsrate Beute rot (r)",
+                    sliderInput(shiny::NS(id, "growth_rate"),
+                                "Wachstumsrate Beute (r)",
                                 value = 5, min = 0.0, max = 12, step = 0.1 ),
-                    sliderInput(shiny::NS(id, "loss_rate_red"),
-                                "Schwundrate Räuber rot (m)",
+                    sliderInput(shiny::NS(id, "loss_rate"),
+                                "Schwundrate Räuber (m)",
                                 value = 10, min = 0.0, max = 20, step = 0.1 )),
              column(4,
-                    sliderInput(shiny::NS(id, "success_rate_red"),
-                                "Jagderfolg rot (a)",
+                    sliderInput(shiny::NS(id, "success_rate"),
+                                "Jagderfolg (a)",
                                 value = 0.1, min = 0.0, max = 3, step = 0.1 ),
-                    sliderInput(shiny::NS(id, "conversion_rate_red"),
-                                "Umwandlungsrate Räuber rot (b)",
+                    sliderInput(shiny::NS(id, "conversion_rate"),
+                                "Umwandlungsrate Räuber (b)",
                                 value = 1.3, min = 0.0, max = 3, step = 0.1 )),
              column(4,
-                    sliderInput(shiny::NS(id, "start_prey_red"),
-                                "Anfangspopulation Beute rot",
-                                value = 40, min = 10, max = 1000, step = 10),
-                    sliderInput(shiny::NS(id, "start_pred_red"),
-                                "Anfangspopulation Raüber rot",
-                                value = 90, min = 10, max = 1000, step = 10))),
+                    sliderInput(shiny::NS(id, "start_prey"),
+                                "Anfangspopulation Beute",
+                                value = 40, min = 10, max = 200, step = 5),
+                    sliderInput(shiny::NS(id, "start_pred"),
+                                "Anfangspopulation Räuber",
+                                value = 90, min = 10, max = 200, step = 5))),
            fluidRow(
              column(12, plotOutput(NS(id, "population")))),
            fluidRow(
@@ -68,30 +66,6 @@ lotkaVolterraUI <- function(id){
              column(4,
                 sliderInput(shiny::NS(id, "end"), "Endzeitpunkt (t)",
                        value = 3, min = 1, max = 10, step = 0.05))),
-           fluidRow(
-             column(4,
-                    sliderInput(shiny::NS(id, "growth_rate_blue"),
-                                "Wachstumsrate Beute blau (r)",
-                                value = 5, min = 0.0, max = 12, step = 0.1 ),
-                    sliderInput(shiny::NS(id, "loss_rate_blue"),
-                                "Schwundrate Räuber blue (m)",
-                                value = 10, min = 0.0, max = 20, step = 0.1 )),
-             column(4,
-                    sliderInput(shiny::NS(id, "success_rate_blue"),
-                                "Jagderfolg blau (a)",
-                                value = 0.1, min = 0.0, max = 3, step = 0.1 ),
-                    sliderInput(shiny::NS(id, "conversion_rate_blue"),
-                                "Umwandlungsrate Räuber blau (b)",
-                                value = 1.3, min = 0.0, max = 3, step = 0.1 )),
-             column(4,
-                    sliderInput(shiny::NS(id, "start_prey_blue"),
-                                "Anfangspopulation Beute blue",
-                                value = 40, min = 10, max = 1000, step = 10),
-                    sliderInput(shiny::NS(id, "start_pred_blue"),
-                                "Anfangspopulation Raüber blau",
-                                value = 90, min = 10, max = 1000, step = 10))),
-
-
            fluidRow(
              column(1, actionButton(NS(id, "code_button"), "Show / Hide Function Code",
                                     onClick="show_hide('lotka_function')"))),
@@ -103,68 +77,68 @@ lotkaVolterraUI <- function(id){
 
 
 lotkaVolterraServer <-  function(id){
+  get_fixed_axis_limits <- function(vals){
+    max_pop <- max(vals)
+    if (max_pop < 200){
+      200
+    } else if(max_pop < 500){
+      500
+    } else if (max_pop < 1000){
+      1000
+    } else {
+      max_pop
+    }
+  }
+
+
   moduleServer(id, function(input, output, session){
-     pop_blue <- reactive({
-        Pars <- c(a = input$success_rate_blue,
-                  b = input$conversion_rate_blue,
-                  r = input$growth_rate_blue,
-                  m = input$loss_rate_blue)
-        State <- c(B = input$start_prey_blue,
-                   R = input$start_pred_blue)
-        Time <- seq(0, input$end, by = 0.01)
-        out <- as.data.frame(deSolve::ode(func = LotVmod, y = State, parms = Pars, times = Time))
-        names(out) <- c("Zeit", "Beute", "Räuber")
-        out$Modell <- "blau"
-        out
-    })
-     pop_red <- reactive({
-       Pars <- c(a = input$success_rate_red,
-                 b = input$conversion_rate_red,
-                 r = input$growth_rate_red,
-                 m = input$loss_rate_red)
-       State <- c(B = input$start_prey_red,
-                  R = input$start_pred_red)
+     pop <- reactive({
+       Pars <- c(a = input$success_rate,
+                 b = input$conversion_rate,
+                 r = input$growth_rate,
+                 m = input$loss_rate)
+       State <- c(B = input$start_prey,
+                  R = input$start_pred)
        Time <- seq(0, input$end, by = 0.01)
        out <- as.data.frame(deSolve::ode(func = LotVmod, y = State, parms = Pars, times = Time))
        names(out) <- c("Zeit", "Beute", "Räuber")
-       out$Modell <- "rot"
        out
      })
     output$population <- renderPlot({
-      out <- pop_blue()
-      out <- dplyr::bind_rows(out, pop_red())
+      out <- pop()
+      axis_beute <- get_fixed_axis_limits(out$Beute)
+      axis_raeuber <- get_fixed_axis_limits(out$Räuber)
       out <- tidyr::pivot_longer(data=out,
                                  cols = c("Beute", "Räuber"),
                                  names_to = "Rolle",
                                  values_to = "Population")
       ggplot2::ggplot(out, ggplot2::aes(x = Zeit, y = Population,
-                                        lty = Rolle, colour=Modell)) +
-      ggplot2::geom_line() +
-      ggplot2::scale_colour_manual(values = c("blue", "red"))
+                                        lty = Rolle)) +
+      ggplot2::geom_line(colour="red") +
+      ggplot2::ylim(0, max(axis_beute, axis_raeuber))
+
 
     })
     output$phase <- renderPlot({
-      out <- pop_blue()
-      out <- dplyr::bind_rows(out, pop_red())
-      vert_blue <- input$growth_rate_blue/input$success_rate_blue
-      vert_red <- input$growth_rate_red/input$success_rate_red
-      hor_blue <- input$loss_rate_blue / (input$success_rate_blue *
-                                         input$conversion_rate_blue)
-      hor_red <- input$loss_rate_red / (input$success_rate_red *
-                                        input$conversion_rate_red)
+      out <- pop()
+      vert <- input$growth_rate/input$success_rate
+      hor <- input$loss_rate / (input$success_rate *
+                                        input$conversion_rate)
 
-      ggplot2::ggplot(out, ggplot2::aes(x = Beute, y = Räuber,
-                                          colour=Modell)) +
-        ggplot2::geom_path() +
-        ggplot2::geom_hline(yintercept = hor_blue, colour = "blue", lty = 2) +
-        ggplot2::geom_hline(yintercept = hor_red, colour = "red", lty = 2) +
-        ggplot2::geom_vline(xintercept = vert_blue, colour = "blue", lty = 2) +
-        ggplot2::geom_vline(xintercept = vert_red, colour = "red", lty = 2) +
-        ggplot2::scale_colour_manual(values = c("blue", "red"))
+      ggplot2::ggplot(out, ggplot2::aes(x = Beute, y = Räuber)) +
+        ggplot2::geom_path(colour="red") +
+        ggplot2::geom_hline(yintercept = hor, colour = "red", lty = 2) +
+        ggplot2::geom_vline(xintercept = vert, colour = "red", lty = 2) +
+        ggplot2::xlim(0, get_fixed_axis_limits(out$Beute)) +
+        ggplot2::ylim(0, get_fixed_axis_limits(out$Räuber))
 
     })
+
+
     lotka_function_text<-
+
       "
+# ermittelt anhand Zustand und Parametern Zuwachs von Räuber(dR) und Beute(dB)
 LotVmod <- function (Time, State, Pars) {
   # with ermöglicht einem Code-Fragment Zugriff auf Variablen,
   # die in einer Liste oder einem Dataframe gespeichert sind
@@ -176,6 +150,13 @@ LotVmod <- function (Time, State, Pars) {
     return(list(c(dB, dR)))
   })
 }
+
+
+# später wird die LotVMod als Funktion die eine Ableitung liefert interpretiert
+# und ein Solver für Differentialgleichung auf sie losgelassen
+
+out <- deSolve::ode(func = LotVmod, y = State, parms = Pars, times = Time)
+
 "
     output$lotka_function_text <- renderText(lotka_function_text);
 
